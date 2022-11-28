88dbf6d8694837daa0f95a29cd79bc59ab74881c
Роман <stroganova_elenka@mail.ru>
2022-11-28 19:16:43 +0300

    Проект сайта

---
+++ src/main/java/com/example/project/york/ProjectYorkApplication.java
@@ -0,0 +1,13 @@
+package com.example.project.york;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectYorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectYorkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/project/york/ProjectYorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project.york;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectYorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
